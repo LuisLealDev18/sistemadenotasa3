a054ff43ba57d2e334702b74f688d5cf559a21ba
Jeffesson Gomes <jeffessongomes@MacBook-Air-de-Jeffesson.local>
2025-11-25 11:54:23 -0300

    feature: configuring fastify and prisma

--- frontend/logout.js
+++
@@ -1,5 +0,0 @@
-// Lógica para o botão de logout
-document.getElementById('logoutBtn').addEventListener('click', function() {
-  // Redireciona para a página de login
-  window.location.href = 'login.html';  // Redirecionando para a página de login
-});
