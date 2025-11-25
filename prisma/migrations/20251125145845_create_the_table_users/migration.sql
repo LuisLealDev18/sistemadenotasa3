5d34567a2d0753b58b1d78a369a561cca34cd0ee
Jeffesson Gomes <jeffessongomes@MacBook-Air-de-Jeffesson.local>
2025-11-25 11:59:44 -0300

    feature: creating the table Users

---
+++ prisma/migrations/20251125145845_create_the_table_users/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
+    "telephone" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "course" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_cpf_key" ON "users"("cpf");
